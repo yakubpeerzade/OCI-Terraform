3f9451f0f42e3607648720a03b773f479bccd524
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-05-14 16:00:22 +0530

    updated DRG configuration

--- terraform-oci-network/environments/dev/outputs.tf
+++ terraform-oci-network/environments/dev/outputs.tf
@@ -1,5 +1,5 @@
 output "drg_id" {
-  value = module.dev_drg.drg_id
+  value = var.drg_id # FIXED Reference
 }
 
 output "hub_vcn_id" {
@@ -14,25 +14,25 @@ output "spoke_vcn_id" {
   value = module.spoke_vcn.vcn_id
 }
 
-output "hub_firewall_subnet_id" {
-  value = module.hub_firewall_subnet.subnet_id
-}
+# output "hub_firewall_subnet_id" {
+#   value = module.hub_firewall_subnet.subnet_id
+# }
 
 output "hub_management_subnet_id" {
   value = module.hub_management_subnet.subnet_id
 }
 
-output "hub_shared_services_subnet_id" {
-  value = module.hub_shared_services_subnet.subnet_id
-}
+# output "hub_shared_services_subnet_id" {
+#   value = module.hub_shared_services_subnet.subnet_id
+# }
 
 output "dmz_public_lb_subnet_id" {
   value = module.dmz_public_lb_subnet.subnet_id
 }
 
-output "dmz_web_subnet_id" {
-  value = module.dmz_web_subnet.subnet_id
-}
+# output "dmz_web_subnet_id" {
+#   value = module.dmz_web_subnet.subnet_id
+# }
 
 output "spoke_app_subnet_id" {
   value = module.spoke_app_subnet.subnet_id
