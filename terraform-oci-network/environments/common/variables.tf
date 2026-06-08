745b079c4f3f7a89fb7001b173820fd555fbc32c
yakubpeerzade <yakubpeerzade123@gmail.com>
2026-06-08 16:38:04 +0530

    added OCI connectivity

--- terraform-oci-network/environments/common/variables.tf
+++ terraform-oci-network/environments/common/variables.tf
@@ -2,3 +2,8 @@ variable "compartment_id" {
   type        = string
   description = "The OCID of the target compartment"
 }
+
+variable "region" {
+  type        = string
+  description = "The OCI region where infrastructure will be built"
+}
